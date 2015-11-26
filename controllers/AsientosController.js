dcd1f018617c6d86c5d1e637b36b03e8a12112c8
lmanganosuarez <manganosuarezl@outlook.com.ar>
2015-11-25 22:19:17 -0300

    Se modifica controlador y modelo 'Asiento'

--- controllers/AsientosController.js
+++ controllers/AsientosController.js
@@ -11,12 +11,45 @@ router.get('/index', function(req, res){
 router.get('/', function(req, res){
     Asiento.find(function(err, result){
         if(!err){
-            res.json(result);
+            return res.json(result);
         } else {
             console.log(JSON.stringify(err));
-            res.json();
         }
     });
+
+    return res.json();
+});
+
+router.post('/', function(req, res){
+    var doc = req.body;
+    var debe = 0;
+    var haber = 0;
+
+    for(var item in doc.items){
+        debe += item.deb;
+        haber += item.hab;
+    }
+
+    if(debe == haber && debe > 0 && haber > 0){
+        doc.val = false;
+        new Asiento(doc, function(err, result){
+            if(err) console.log(JSON.stringify(err));
+            else return res.json(result);
+        });
+    } else {
+        console.log("Asiento no válido.");
+    }
+
+    return res.json();
+});
+
+router.post('/validate/:id', function(req, res){
+    Asiento.findOne({_id: req.params.id}, {val: true}, function(err, result){
+        if(err) console.log(JSON.stringify(err));
+        else return res.json(result);
+    });
+
+    return res.json();
 });
 
 module.exports = router;
--- models/Asiento.js
+++ models/Asiento.js
@@ -4,12 +4,14 @@
 var db = require('../db');
 
 module.exports = db.model('Asiento', {
-    fec: db.Schema.Types.Date,
-    items: [
+    fec: db.Schema.Types.Date,  // Fecha
+    val: db.Schema.Types.Boolean,   // Válido
+    des: db.Schema.Types.String,    // Descripción
+    items: [    // Estructura del asiento
         {
-            cue: db.Schema.Types.String,
-            deb: db.Schema.Types.Number,
-            hab: db.Schema.Types.Number
+            cue: db.Schema.Types.String,    // Cuenta
+            deb: db.Schema.Types.Number,    // Debe
+            hab: db.Schema.Types.Number     // Haber
         }
     ]
 });
