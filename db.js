643438aad9072c7f5dc8adbeef4cf79a9c9aa1d8
lmanganosuarez <manganosuarezl@outlook.com.ar>
2015-11-25 21:24:01 -0300

    Se modifica el archivo 'db.js'

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 /**
  * Created by manga_000 on 21/11/2015.
  */
-var db = require('cassandra-driver');
-var client = new db.Client({contactPoints: ['127.0.0.1:9042'], keyspace: 'JacklsoftERP'});
+var db = require('mongoose');
+db.connect('localhost', 'jacklsoft-erp', 27017, {
+    user: 'admin',
+    pass: 'entrar',
+    auth: {
+        authdb: 'admin'
+    }
+});
 
-module.exports = client;
+module.exports = db;
