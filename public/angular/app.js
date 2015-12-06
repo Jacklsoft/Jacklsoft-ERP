bafc79d210e9475a7a5b07922c57e3ec24c1bf66
lmanganosuarez <manganosuarezl@outlook.com.ar>
2015-12-06 08:48:13 -0300

    Se elimina la librería ngJacklsoft

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by manga_000 on 23/11/2015.
  */
-var angularJERP = angular.module('angularJERP', ['ngJacklsoft']);
+var angularJERP = angular.module('angularJERP', ['ui.bootstrap', 'ngAnimate', 'angular.filter']);
