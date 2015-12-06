9624420c9f6ffbbfd5aed221dca01c491f1cc29f
lmanganosuarez <manganosuarezl@outlook.com.ar>
2015-12-06 09:26:07 -0300

    Se crea interface del partial 'InversionForm'

--- public/angular/app.js
+++ public/angular/app.js
@@ -2,3 +2,15 @@
  * Created by manga_000 on 23/11/2015.
  */
 var angularJERP = angular.module('angularJERP', ['ui.bootstrap', 'ngAnimate', 'angular.filter']);
+
+angularJERP.directive('jerpDateModel', function(){
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elem, attr, modelCtrl){
+            modelCtrl.$formatters.push(function(modelValue){
+                return (modelValue == null ? null : new Date(modelValue));
+            });
+        }
+    };
+});
---
+++ public/angular/views/InversionForm/InversionForm.js
@@ -0,0 +1,12 @@
+/**
+ * Created by manga_000 on 06/12/2015.
+ */
+angularJERP.directive('jerpInversionForm', function(){
+    return {
+        restrict: 'A',
+        templateUrl: '/angular/views/InversionForm/InversionForm.html',
+        controller: function($scope){
+
+        }
+    };
+});
